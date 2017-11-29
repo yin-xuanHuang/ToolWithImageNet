817c1effcb19fcd07ba5a1565567476fe16e9cfa
yinxuanh <yinxuanh@gmail.com>
2017-11-29 21:49:07 +0800

    Add stepFour process

---
+++ src/sample/CleanImages.java
@@ -0,0 +1,107 @@
+package sample;
+
+import javafx.concurrent.Task;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class CleanImages extends Task<Void> {
+//    TODO 效能問題
+//    算出共有幾張圖，以此為progressBar total。
+
+    private Queue<String> shareQueue;
+    private final String mainDirName;
+    private final String projectDirName;
+
+    private final String imageDirName;
+    private final ArrayList<String> imageSubDirsName;
+
+    private long countFiles = 0;
+    private long totalFiles;
+    private long countDeleteFiles = 0;
+
+    public CleanImages(String mainDirName,
+                       String projectDirName,
+                       String imageDirName,
+                       String imageMatchedDirName,
+                       String imageUnmatchedDirName) {
+
+        this.mainDirName = mainDirName;
+        this.projectDirName = projectDirName;
+        this.imageDirName = imageDirName;
+
+        imageSubDirsName = new ArrayList<>();
+        imageSubDirsName.add(imageMatchedDirName);
+        imageSubDirsName.add(imageUnmatchedDirName);
+
+        shareQueue = new ArrayDeque<>();
+    }
+
+    @Override
+    protected Void call() throws Exception {
+
+        totalFiles = getTotalFiles();
+
+        Path imagePath = FileSystems.getDefault().getPath(mainDirName,
+                                                        projectDirName,
+                                                        imageDirName);
+
+
+        new Thread(() -> {
+            try {
+//                這個如何stop?
+                Files.walkFileTree(imagePath, new WalkingFileTree(shareQueue));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        while(true){
+            Thread.sleep(500);
+            if(updateUIMassage())
+                break;
+            if(isCancelled()){
+                break;
+            }
+        }
+        return null;
+    }
+
+    private long getTotalFiles() {
+
+        long totalFiles = 0;
+
+        Path imageDirPath = FileSystems.getDefault().getPath(mainDirName, projectDirName, imageDirName);
+
+        for(File imageSubDir: imageDirPath.toFile().listFiles()) {
+            totalFiles += imageSubDir.list().length;
+        }
+
+        return totalFiles;
+    }
+
+    private boolean updateUIMassage() {
+
+        if(!shareQueue.isEmpty()){
+            String massage = shareQueue.poll();
+            if(massage.equals("delete")){
+                countDeleteFiles++;
+            } else {
+                updateProgress(this.countFiles++, this.totalFiles);
+            }
+            updateMessage( " " + countFiles + " / " + totalFiles + " [ " + countDeleteFiles + " ]");
+        }
+
+        if(this.countFiles == this.totalFiles)
+            return true;
+        else
+            return false;
+
+    }
+}
---
+++ src/sample/WalkingFileTree.java
@@ -0,0 +1,172 @@
+package sample;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Queue;
+
+
+public class WalkingFileTree extends SimpleFileVisitor<Path> {
+
+//    轉檔成一致的圖檔(ex. jpg)
+//    identify(remove missing image)
+
+    private Queue<String> shareQueue;
+    private final String wrongImageDirName = "wrong_images";
+    private Path wrongImageDirPath;
+    private File[] wrongImageList;
+    private FileVisitResult fileVisitResult;
+
+    public WalkingFileTree(Queue<String> shareQueue) {
+        wrongImageDirPath = FileSystems.getDefault().getPath(wrongImageDirName);
+        wrongImageList = wrongImageDirPath.toFile().listFiles();
+        this.shareQueue = shareQueue;
+        fileVisitResult = FileVisitResult.CONTINUE;
+    }
+
+    public void stop() {
+        fileVisitResult = FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+//        非jpg檔轉檔成jpg並加附檔名，jpg檔則增加副檔名
+//        原檔刪除
+        Path out = FileSystems.getDefault().getPath(file.getParent().toString(),
+                                            file.getFileName() + ".jpg");
+
+        String type = file.toFile().toURI().toURL().openConnection().getContentType();
+        if(!type.split("/")[1].equals("jpeg")){
+            image2jpeg(file, out);
+        } else {
+            Files.copy(file, out, StandardCopyOption.REPLACE_EXISTING);
+        }
+        Files.delete(file);
+
+
+//        check isMissingImage()
+//        list wrong image directory's file and compare to out jpg file.
+
+        Boolean isWrongImage = false;
+
+        if(wrongImageList != null) {
+            isWrongImage = isMissingImage(out);
+        } else {
+            System.out.println("No wrong image in " + wrongImageDirName + " directory!");
+        }
+
+        if(isWrongImage) {
+            Files.delete(file);
+            System.out.println("Delete a wrong image.");//debug
+            shareQueue.add("delete");
+        }
+
+        shareQueue.add("total");
+
+        return fileVisitResult;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println(dir.toAbsolutePath());
+        return fileVisitResult;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file: " + file.toAbsolutePath() + " " + exc.getMessage());
+        System.out.println("===================================");
+        return fileVisitResult;
+    }
+
+//    轉檔案格式
+    private void image2jpeg(Path in, Path out) {
+        try (InputStream is = new FileInputStream(in.toFile())) {
+            ImageInputStream iis = ImageIO.createImageInputStream(is);
+            BufferedImage image = ImageIO.read(iis);
+            try (OutputStream os = new FileOutputStream(out.toFile())) {
+                ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+                ImageIO.write(image, "jpg", ios);
+            } catch (Exception exp) {
+                exp.printStackTrace();
+            }
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+    }
+
+    /**
+     * Compares two images pixel by pixel.
+     *
+     * @param imgA the first image.
+     * @param imgB the second image.
+     * @return whether the images are both the same or not.
+     */
+    private boolean compareImages(BufferedImage imgA, BufferedImage imgB) {
+        // The images must be the same size.
+        if (imgA.getWidth() == imgB.getWidth() && imgA.getHeight() == imgB.getHeight()) {
+            int width = imgA.getWidth();
+            int height = imgA.getHeight();
+
+            // Loop over every pixel.
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    // Compare the pixels for equality.
+                    if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+
+//    效能有點低
+//    用 compareImages() 來比對每一個wrong image
+    private boolean isMissingImage(Path beCheckedPath) {
+
+        try (InputStream beCheckedIS = new FileInputStream(beCheckedPath.toFile())) {
+            ImageInputStream beCheckedIIS = ImageIO.createImageInputStream(beCheckedIS);
+            BufferedImage beCheckedBI = ImageIO.read(beCheckedIIS);
+
+            for(File wrongImage: wrongImageList) {
+                try (InputStream wrongImageIS = new FileInputStream(wrongImage)) {
+                    ImageInputStream wrongImageIIS  = ImageIO.createImageInputStream(wrongImageIS);
+                    BufferedImage wrongImageBI = ImageIO.read(wrongImageIIS);
+
+                    if(compareImages(beCheckedBI, wrongImageBI)) {
+                        return true;
+                    }
+
+                } catch (Exception exp) {
+                    exp.printStackTrace();
+                }
+            }
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            try {
+                Files.delete(beCheckedPath);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+}
